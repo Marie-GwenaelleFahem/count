36c6367db2e9c67e128e7c669bb7371b83648d61
mgfahem <mgfahem@gmail.com>
2023-09-13 18:55:05 +0200

    Add new version

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const money = ref(0)
+
+function addMoney() {
+  money.value++
+}
+
 </script>
 
 <template>
+  <video autoplay loop muted class="video">
+    <source src="./assets/hide_da_money_yall.mp4" type="video/mp4">
+  </video>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <h1>Click down below to hide money !</h1>
+      <button @click="addMoney">Money hidden : {{ money }} $</button>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
@@ -26,60 +27,44 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
 @media (min-width: 1024px) {
+
+  .video {
+    z-index: -1000; 
+    left: 50%;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 50%;
+    --webkit-transform: translate(-50% , -50%);
+    --moz-transform: translate(-50% , -50%);
+    transform: translate(-50% , -50%);
+    object-fit: cover;
+  }
+
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
   header .wrapper {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    justify-content: center;
+    flex-direction: column;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .wrapper h1 {
+    color: white;
   }
+
+  .wrapper button {
+    margin-top: 30px;
+    width: fit-content;
+    padding: 20px;
+    color: none;
+    border: 2px solid white;
+  }
+
 }
 </style>
